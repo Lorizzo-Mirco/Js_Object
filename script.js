fce95ce23de4be79fecc14174799c8594806e227
Mirco Lorizzo <45140218+Lorizzo-Mirco@users.noreply.github.com>
2025-01-31 10:27:17 +0000

    Aggiunto audio animali

--- script.js
+++ script.js
@@ -7,13 +7,17 @@ function Visualizza_Animale(animale) {
         document.getElementById("animale-specie").innerHTML = "LA SPECIE E': " + animale1.specie;
         document.getElementById("animale-razza").innerHTML = "LA RAZZA E': " + animale1.razza;
         document.getElementById("animale-zampe").innerHTML = "HA " + animale1.zampe + " ZAMPE";
+        var audio = new Audio('audios/Mucca.mp3');
     } else if (animale == 1) {
         document.getElementById("animale-specie").innerHTML = "LA SPECIE E': " + animale2.specie;
         document.getElementById("animale-razza").innerHTML = "LA RAZZA E': " + animale2.razza;
         document.getElementById("animale-zampe").innerHTML = "HA " + animale2.zampe + " ZAMPE";
+        var audio = new Audio('audios/Cane.mp3');
     } else if (animale == 2) {
         document.getElementById("animale-specie").innerHTML = "LA SPECIE E': " + animale3["specie"];
         document.getElementById("animale-razza").innerHTML = "LA RAZZA E': " + animale3["razza"];
         document.getElementById("animale-zampe").innerHTML = "HA " + animale3["zampe"] + " ZAMPE";
+        var audio = new Audio('audios/Gallina.mp3');
     }
+    audio.play();
 }
